ea24a9b2564d52d67abb7129f4a28546dd0c6cef
sergiogyoz <sergiogyoz@hotmail.com>
2022-05-07 16:18:05 -0500

    No-syntax errors testing done
    added paper doc

--- Main.py
+++ Main.py
@@ -17,7 +17,8 @@ import scipy.sparse.linalg
 class InitialConditions:
     """
     Class to store initial conditions of a Network problem and
-    store frequenly used constants for the network.
+    store frequenly used constants for the network. Make sure 
+    to use np.arrays with specified dtype
 
     ...
 
@@ -25,7 +26,7 @@ class InitialConditions:
     ----------
     qx : numpy 3D array
         Initial condition. For i,j it contains the resource
-        distribution at t=0, i.e. qij(x_n,0) for 0<n<=N[i][j]
+        distribution at t=0, i.e. qij(x_n,0) for 0<n<=N[i,j]
     l : numpy matrix
         Lenghts of each edge i,j
     R : numpy matrix
@@ -43,10 +44,10 @@ class InitialConditions:
     adj : list of lists
         Adjacency matrix to efficiently tranverse the matrix.
         List entry i is a list of the indices j connected to i.
-    qt : vector of functions
+    qt : (not implemented, optional) vector of functions
         Boundary condition. Resource distribution qij(0,t)=Sij*Ci(t)
         for each node i over time t
-    C : (optional) vector of functions
+    C : (not implemented, optional) vector of functions
         Not currently implemented. Concentrations functions
 
     Methods
@@ -63,36 +64,66 @@ class InitialConditions:
     evalvector(vector, t=0, printvector=False)
         Evaluates a vector function at t
     """
-    def __init__(self, qx, l, R, D, u, S, I,
-                 N, adj, qt, C=False):
-        self.qx = qx    #Array of q at every Delta x when t=0
+    def __init__(self, qx=np.zeros(0), l=np.zeros(0), R=np.zeros(0),
+                 D=np.zeros(0), u=np.zeros(0), S=np.zeros(0), I=np.array([1]),
+                 qt=False, C=False):
+        self.qx = qx    # 3D Array of qij at every Delta x at current t
         self.l = l
         self.R = R
         self.D = D
         self.u = u
         self.S = S
         self.I = I
-        self.N = N  #this could be optional
-        self.adj = adj    #this could be optional
-        self.qt = qt    #This is also optional given I
+        self.n = len(l)
+
+        self.N = np.zeros([self.n, self.n], dtype=np.int32)
+        self.adj = [ [] for _ in range(self.n)]
+        for i in range(self.n):
+            for j in range(self.n):
+                self.N[i,j] = len(qx[i,j])
+                if l[i,j]>0:
+                    self.adj[i].append(j)
+        self.g = np.divide(u*l, 2*D, out=np.zeros_like(u), where=(D != 0))
+
+        self.qt = qt    #This should be optional given I
         self.C = C  #not currently implemented
         if (not C) and (not I):
             raise(ValueError("No concentration C or current I given"))
-        if not I:
-            self.n = len(C)
-        else:
-            self.n = len(I)
-        self.g = u*l/(2*D)
+
+    def print_IC(self, t=0.0, stop_at=3, show_extra=False):
+        print("Number of nodes= ", self.n)
+        extra_text=""
+        if self.n>stop_at:
+            extra_text=" ... "
+        print("l= ")
+        print( self.l[:stop_at, :stop_at], extra_text)
+        print("R= ")
+        print( self.R[:stop_at, :stop_at], extra_text)
+        print("D= ")
+        print( self.D[:stop_at, :stop_at], extra_text)
+        print("u= ")
+        print( self.u[:stop_at, :stop_at], extra_text)
+        print("S= ")
+        print( self.S[:stop_at, :stop_at], extra_text)
+        print("I= ")
+        I_mat=InitialConditions.evalvector(self.I, t)
+        print( I_mat[:stop_at], extra_text)
+        if show_extra:
+            print("N= ")
+            print( self.N[:stop_at, :stop_at], extra_text)
+            print("Adjacency list= ")
+            print( self.adj[:stop_at], extra_text)
+
 
     @staticmethod
     def antisymmetrize(matrix, lowerleft=False):
-        n = len(n)
+        n = len(matrix)
         for i in range(n):
             for j in range(i+1, n):
                 if not lowerleft:
-                    matrix[j][i] = -matrix[i][j]
+                    matrix[j,i] = -matrix[i,j]
                 else:
-                    matrix[i][j] = -matrix[j][i]
+                    matrix[i,j] = -matrix[j,i]
 
     @staticmethod
     def symmetrize(matrix, lowerleft=False):
@@ -100,25 +131,25 @@ class InitialConditions:
         for i in range(n):
             for j in range(i+1, n):
                 if not lowerleft:
-                    matrix[j][i] = matrix[i][j]
+                    matrix[j,i] = matrix[i,j]
                 else:
-                    matrix[i][j] = matrix[j][i]
+                    matrix[i,j] = matrix[j,i]
 
     # useful functions for matrix and vector evaluation
     @staticmethod
-    def evalmatrix(matrix, t=0, printmatrix=False):
-        n = len(matrix)
-        m = len(matrix[0])
+    def evalmatrix(matrix_func, t=0.0, printmatrix=False):
+        n = len(matrix_func)
+        m = len(matrix_func[0])
         mat = np.zeros([n, m])
         for i in range(n):
             for j in range(m):
-                mat[i, j] = matrix[i][j](t)
+                mat[i, j] = matrix_func[i][j](t)
         if printmatrix:
             print(mat)
         return mat
 
     @staticmethod
-    def evalvector(vector, t=0, printvector=False):
+    def evalvector(vector, t=0.0, printvector=False):
         n = len(vector)
         vec = np.zeros(n)
         for i in range(n):
@@ -199,7 +230,6 @@ class f_tools:
 class Solver:
     ic = InitialConditions()
     ftool = f_tools()
-
     def __init__(self, initial_conditions):
         self.ic = initial_conditions
         self.ftool = f_tools()
@@ -211,53 +241,53 @@ class Solver:
             I_lap[i] = self.ftool.Laplace(self.ic.I[i], s)
         return I_lap
 
-    def prog_matrix(self, s, t=0, return_extra=False):
+    def prog_matrix(self, s, t=0.0, return_extra=False):
         n = self.ic.n
         alpha = np.zeros([n, n])
         h = np.zeros([n, n])
         for i in range(n):
-            for j in range(n):
-                alpha[i][j] = math.sqrt(self.ic.u[i][j]*self.ic.u[i][j]
-                                        + 4*self.ic.D[i][j]*(s+self.ic.R[i][j]))
-                h[i][j] = alpha[i][j]*self.ic.l[i][j]/(2*self.ic.D[i][j])
+            for j in self.ic.adj[i]:
+                alpha[i,j] = math.sqrt(self.ic.u[i,j]*self.ic.u[i,j]
+                                        + 4*self.ic.D[i,j]*(s+self.ic.R[i,j]))
+                h[i,j] = alpha[i,j]*self.ic.l[i,j]/(2*self.ic.D[i,j])
         M = np.zeros([n, n])
         for i in range(n):
             for j in range(n):
                 if i == j:
                     sum = 0
                     for k in range(n):
-                        sum += self.ic.S[i][k]*(self.ic.u[i][k]/2 + alpha[i][k]/(2*math.tanh(h[i][j])))
+                        sum += self.ic.S[i,k]*(self.ic.u[i,k]/2 + alpha[i,k]/(2*math.tanh(h[i,k])))
                     M[i][j] = sum
                 else:
-                    M[i][j] = (-self.ic.S[i][j]
-                               * alpha[i][j]
-                               * math.exp(-self.ic.g[i][j])
-                               / (2*math.sinh(h[i][j])))
+                    M[i][j] = (-self.ic.S[i,j]
+                               * alpha[i,j]
+                               * math.exp(-self.ic.g[i,j])
+                               / (2*math.sinh(h[i,j])))
         if not return_extra:
             return M
         else:
             return M, alpha, h
 
-    def beta_matrix(self, s, t=0, return_extra=False):
+    def beta_matrix(self, s, t=0.0, return_extra=False):
         n = self.ic.n
         M, alpha, h = self.prog_matrix(s, t, True)
         b = np.zeros([n, n])
         for i in range(n):
             for j in range(n):
                 b[i][j] = Solver.beta(s,
-                                      self.ic.qx[i][j],
-                                      self.ic.g[i][j],
-                                      h[i][j],
-                                      R[i][j],
-                                      self.ic.u[i][j],
-                                      alpha[i][j],
-                                      self.ic.N[i][j])
+                                      self.ic.qx[i,j],
+                                      self.ic.g[i,j],
+                                      h[i,j],
+                                      R[i,j],
+                                      self.ic.u[i,j],
+                                      alpha[i,j],
+                                      self.ic.N[i,j])
         if not return_extra:
             return b
         else:
             return b, M, alpha, h
 
-    def homogeneous_lap(self, x_range, t, Omega=10):
+    def homogeneous_lap(self, t, Omega=10):
         log2 = self.ftool.LOG2
         ndim = self.ic.n
         # values of s for GS laplace inversion
@@ -275,19 +305,20 @@ class Solver:
             # fill the X_ij(s)
             for i in range(ndim):
                 for j in self.ic.adj[i]:
-                    Xs[s_ind][i][j] = [C[i]*self.ic.S[i][j]]
+                    Xs[s_ind,i,j] = C[i]*self.ic.S[i,j]
             s_ind = s_ind + 1
         # Find qij(x,t) using stored values and GS algor
 
-        qs = np.zeros([ndim, ndim, self.ic.N[i][j]])
+        qt = np.zeros([ndim, ndim, self.ic.N[i,j]])
         for i in range(ndim):
             for j in self.ic.adj[i]:
-                x_ind = 0
-                for x in x_range:
+                x = 0.0
+                dx = self.ic.l[i,j]/self.ic.N[i,j]
+                for x_ind in self.ic.N[i,j]:
                     Qijxs = [self.Q(i, j, x, s_ind, Xs, hs) for s_ind in range(Omega)]
-                    qs[i][j][x_ind] = self.ftool.GSinverse(Qijxs, t, Omega)
-                    x_ind = x_ind+1
-
+                    qt[i,j,x_ind] = self.ftool.GSinverse(Qijxs, t, Omega)
+                    x = x+dx
+        return qt
 
 
 
@@ -306,123 +337,69 @@ class Solver:
         return sum
 
     def Q(self, i, j, x, s_ind, X, h):
-        coeff1 = self.ic.l[i][j]-x/self.ic.l[i][j]
-        coeff2 = x/self.ic.l[i][j]
-        Sol = (X[s_ind][i][j]*(math.sinh(coeff1*h[s_ind][i][j])
-                               / math.sinh(h[s_ind][i][j])
+        coeff1 = self.ic.l[i,j]-x/self.ic.l[i,j]
+        coeff2 = x/self.ic.l[i,j]
+        Sol = (X[s_ind,i,j]*(math.sinh(coeff1*h[s_ind,i,j])
+                               / math.sinh(h[s_ind,i,j])
                                )
-               * math.exp(coeff2*self.ic.g[i][j])
-               + X[s_ind][j][i]*(math.sinh(coeff2*h[s_ind][i][j])
-                                 / math.sinh(h[s_ind][i][j])
+               * math.exp(coeff2*self.ic.g[i,j])
+               + X[s_ind,j,i]*(math.sinh(coeff2*h[s_ind,i,j])
+                                 / math.sinh(h[s_ind,i,j])
                                  )
-               * math.exp(-coeff1*self.ic.g[i][j])
+               * math.exp(-coeff1*self.ic.g[i,j])
                )
         return Sol
 
+
 # %%
-# initial conditions setup
+# ----- initial conditions setup
+# parameters to ease change network input
 n = 2
-# We start by saving all the constant coefficients of the network
-l = np.matrix([[0, 1], [1, 0]])
-R = np.matrix([[0, 0.1], [0.1, 0]])
+Ngrid = 10
+uconst = 2
 Dm = 0.1
+uconst = 2
+lconst = 1
+Rconst = 0.1
+Sconst = 0.5
 
-# zero, one, and constant functions
-adjacency = [[0, 1], [-1, 0]]  # adjacency matrix
-u = [[0]*n]*n  # velocities
-S = [[0]*n]*n  # cross sectional areas
-D = [[0]*n]*n  # diffusion outside of the network
-C = [0]*n  # concentration at the node
-# fill U and S
-for i in range(n):
-    for j in range(i, n):
-        def u_ij(t=0):
-            return i+j
+# Networks parameters
+qx = np.zeros([n, n, Ngrid])
+l = np.matrix([[0, lconst], [lconst, 0]], dtype=np.float64)
+R = np.matrix([[0, Rconst], [Rconst, 0]], dtype=np.float64)
+D = np.matrix([[0, Dm], [Dm, 0]], dtype=np.float64)  # diffusion outside of the network
+u = np.matrix([[0, uconst], [uconst, 0]], dtype=np.float64)  # velocities
+S = np.matrix([[0, Sconst], [Sconst, 0]], dtype=np.float64)  # cross sectional areas
 
-        def S_ij(t=0):
-            return 1/2
-
-        u[i][j] = u_ij
-        S[i][j] = S_ij
-
+# Fix a couple of stuff
 InitialConditions.antisymmetrize(u)
 InitialConditions.symmetrize(S)
 
 # fill q_ij(x,0) from 0 to l_ij
-k=0.01
-qx=[[0]*n]*n
+k = 0.0
 for i in range(n):
-    for j in range(i+1,n):
-        def g(x):
-            if (0<=x and x<=l[i][j]):
-                return k
-            else:
-                return 0
-        qx[i][j]=g
+    for j in range(i+1, n):
+        qx[i,j] = [k for x in range(Ngrid)]
 
-# fill q_ij(0,t)
-qt = [f_tools.zerof]*n
-for i in range(n):
-    def q_i(t=0):
-        return t/10
-    qt[i] = q_i
-
-
-# fill D and C
-for i in range(n):
-    for j in range(n):
-        def D_ij(t=0):
-            return Dm + (u[i][j] * u[i][j]) * 1 / (48 * Dm)
-
-        D[i][j] = D_ij
-
-    C[i] = f_tools.zerof
-    for j in range(n):
-        if adjacency[i][j]:
-            def C_i(t=0):
-                return qt(t) / S[i][j]
-            C[i] = C_i
-            break
-
-# if we don't have C we need I_i(t) (net rate resource leaves i)
+# We need I_i(t) (net rate resource leaves i)
 I = [f_tools.zerof]*n
+
+def I_i(t=0):
+    return (n-1)*uconst/10
 for i in range(n):
-    def I_i(t=0):
-        sum = 0
-        for j in range(n):
-            sum = sum+u[i][j]*t/10
-        return sum
     I[i] = I_i
 
-print("u=")
-InitialConditions.evalmatrix(u, printmatrix=True)
-print("S=")
-InitialConditions.evalmatrix(S, printmatrix=True)
-print("D=")
-InitialConditions.evalmatrix(D, printmatrix=True)
-print("q=")
-InitialConditions.evalvector(qx, printvector=True)
-print("I=")
-InitialConditions.evalvector(I, printvector=True)
-
+IC_test=InitialConditions(qx, l, R, D, u, S, I)
+solve=Solver(IC_test)
+q=solve.homogeneous_lap(0.2)
 # %%
-"""A=np.zeros([2,2])
-A[1][1]=1
-A[0][0]=2
-b=np.zeros(2)
-b[1]=5
-b[0]=3.215
-x=scipy.sparse.linalg.spsolve(A,b)
-print(x)
-print(A.dot(x)-b)"""
-# %%
-import numpy as np
+"""import numpy as np
 x=np.zeros([5,2,2])+1
 y=np.zeros([2,2])+3
 z=np.array([[1,2],[4,8]])
 
-print(x[0])
+print(len(x))
 x[0]=y
-print(x[0])
+print(x[0])"""
 
 # %%
